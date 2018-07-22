21746a76de4910c630a2f7e4ff132ebdb23b1024
Federico Bertani <fedbertani@gmail.com>
2018-07-22 16:39:25 +0200

    Some plots to show data preprocessing

--- plots_scripts/data_preprocessing.py
+++ plots_scripts/data_preprocessing.py
@@ -39,45 +39,54 @@ if __name__ == "__main__":
 
     converts_measurement_units(accelerations, np.array([0.1]), gps_speed, coordinates, heading)
 
-    # reduce accelerations disturbance
-    times, accelerations = reduce_disturbance(times, accelerations, window_size)
-    # reduce angular velocities disturbance
-    _, angular_velocities = reduce_disturbance(times, angular_velocities, window_size)
-    gps_speed = gps_speed[round(window_size / 2):-round(window_size / 2)]
     normalize_timestamp(times)
 
-    # get time windows where vehicle is stationary
-    stationary_times = get_stationary_times(gps_speed)
+    xlim= (20,40)
+    ylim= (-1,2)
 
     fig = plt.figure()
     fig1_ax1 = fig.add_subplot(1, 1, 1)
-    fig1_ax1.set_title("Gyroscope offset before removal")
+    fig1_ax1.set_title("Before")
     fig1_ax1.set_xlabel("time (s)")
-    fig1_ax1.set_ylabel("speed (m/s)", color='b')
+    fig1_ax1.set_ylabel("acceleration (m/s²)", color='b')
     fig1_ax1.tick_params("y", color='b')
-    fig1_ax1.plot(times, gps_speed,color='b')
-    ylim = fig1_ax1.get_ylim()
+    fig1_ax1.plot(times, accelerations[1],color='b')
+    fig1_ax1.set_xlim(xlim)
+    fig1_ax1.set_ylim(ylim)
     fig1_ax2 = fig1_ax1.twinx()
     fig1_ax2.set_ylabel("angular velocity (deg/s)", color='r')
     fig1_ax2.tick_params("y", colors='r')
     fig1_ax2.plot(times, -angular_velocities[2], color='r')
     fig1_ax2.set_ylim(ylim)
+    fig1_ax2.set_xlim(xlim)
     plt.legend()
+
+    # reduce accelerations disturbance
+    times, accelerations = reduce_disturbance(times, accelerations, window_size)
+    # reduce angular velocities disturbance
+    _, angular_velocities = reduce_disturbance(times, angular_velocities, window_size)
+    gps_speed = gps_speed[round(window_size / 2):-round(window_size / 2)]
+
+    # get time windows where vehicle is stationary
+    stationary_times = get_stationary_times(gps_speed)
 
     # clear gyroscope drift
     angular_velocities = clear_gyro_drift(angular_velocities, stationary_times)
 
     fig2 = plt.figure()
     fig2_ax1 = fig2.add_subplot(1, 1, 1)
-    fig2_ax1.set_title("Gyroscope offset after removal")
+    fig2_ax1.set_title("After")
     fig2_ax1.set_xlabel("time (s)")
-    fig2_ax1.set_ylabel("speed (m/s)",color='b')
+    fig2_ax1.set_ylabel("acceleration (m/s²)", color='b')
     fig2_ax1.tick_params("y", color='b')
-    fig2_ax1.plot(times, gps_speed)
+    fig2_ax1.plot(times, accelerations[1],color='b')
+    fig2_ax1.set_ylim(ylim)
+    fig2_ax1.set_xlim(xlim)
     fig2_ax2 = fig2_ax1.twinx()
     fig2_ax2.set_ylabel("angular velocity (deg/s)", color='r')
     fig2_ax2.tick_params("y", colors='r')
     fig2_ax2.plot(times, -angular_velocities[2], color='r')
     fig2_ax2.set_ylim(ylim)
+    fig2_ax1.set_xlim(xlim)
     plt.legend()
     plt.show()
