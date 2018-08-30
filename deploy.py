620cf7312e1229f8ea25e9065a771c172a962c28
Federico Bertani <fedbertani@gmail.com>
2018-08-30 10:35:40 +0200

    Documented and enabled compression in deploy script

--- deploy.py
+++ deploy.py
@@ -1,33 +1,66 @@
 #!/usr/bin/env python3
 
+import importlib
+import os
+import shutil
+import zipfile
+
+
 def zipdir(path, ziph):
-    # ziph is zipfile handle
+    """ Utility routine to zip directory recursively
+    :param path: string path of directory to compress
+    :param ziph : zipfile handle
+    """
+
+    # 'walk' inside directory tree listing directories and files into them
     for root, dirs, files in os.walk(path):
+        # for each file in current walked dir
         for file in files:
+            # add file to zip
             ziph.write(os.path.join(root, file))
 
-if __name__=="__main__":
-    import os, zipfile, shutil
 
+if __name__ == "__main__":
+    # get project root
     my_path = os.path.abspath(os.path.dirname(__file__))
+    # path of a temp directory to zip
     path = os.path.join(my_path, "blender_inertial")
-    addon_updater_path = os.path.join(my_path, "blender-addon-updater","addon_updater.py")
+    # prepare other path strings
+    addon_updater_path = os.path.join(my_path, "blender-addon-updater", "addon_updater.py")
     src_full_path = os.path.join(my_path, "src")
-    new_src = os.path.join(path,"src")
+    new_src = os.path.join(path, "src")
     init_full_path = os.path.join(my_path, "__init__.py")
-    requirements_full_path = os.path.join(my_path,"requirements.txt")
+    requirements_full_path = os.path.join(my_path, "requirements.txt")
     addon_updater_ops_path = os.path.join(my_path, "my_addon_updater_ops.py")
-    addon_updater_ops_new_path = os.path.join(path,"addon_updater_ops.py")
+    addon_updater_ops_new_path = os.path.join(path, "addon_updater_ops.py")
     # TODO if dir exist remove it
     os.makedirs(path, exist_ok=True)
-    shutil.copytree(src_full_path,new_src)
+    # copy necessary project file to temp directory
+    shutil.copytree(src_full_path, new_src)
     shutil.copy(init_full_path, path)
     shutil.copy(addon_updater_ops_path, addon_updater_ops_new_path)
     shutil.copy(addon_updater_path, path)
     shutil.copy(requirements_full_path, path)
-    zip_path = os.path.join(my_path,"blender_inertial.zip")
-    addon_zip = zipfile.ZipFile(zip_path, mode='w')
-    # TODO enable compression
-    zipdir("blender_inertial",addon_zip)
+    # final path of zip file
+    zip_path = os.path.join(my_path, "blender_inertial.zip")
+    # crete zip file object
+    # check what zip algorithms modules are installed
+    # priority to algorithms that create smaller zip (time doesn't matter)
+    if importlib.util.find_spec("lzma"):
+        # use lzma
+        addon_zip = zipfile.ZipFile(zip_path, mode='w', compression=zipfile.ZIP_LZMA)
+    elif importlib.util.find_spec("bz2"):
+        # use bz2
+        addon_zip = zipfile.ZipFile(zip_path, mode='w', compression=zipfile.ZIP_BZIP2)
+    elif importlib.util.find_spec("zlib"):
+        # use zlib
+        addon_zip = zipfile.ZipFile(zip_path, mode='w', compression=zipfile.ZIP_DEFLATED)
+    else:
+        # no compression, default option
+        addon_zip = zipfile.ZipFile(zip_path, mode='w')
+    # zip directory
+    zipdir("blender_inertial", addon_zip)
+    # close zip file handle
     addon_zip.close()
+    # remove temporary directory
     shutil.rmtree(path)
