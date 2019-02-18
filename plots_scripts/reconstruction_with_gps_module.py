1d9a5923aa8f3fa746f2143c9feedf52036d5200
Federico Bertani <fedbertani@gmail.com>
2019-02-18 16:21:00 +0100

    Updated reconstruction plot scripts

--- plots_scripts/reconstruction_with_gps_module.py
+++ plots_scripts/reconstruction_with_gps_module.py
@@ -30,7 +30,7 @@ import numpy as np
 
 from src.clean_data_utils import converts_measurement_units, reduce_disturbance, \
     clear_gyro_drift, correct_z_orientation, normalize_timestamp, \
-    get_stationary_times
+    get_stationary_times, truncate_if_crash
 from src.gnss_utils import get_positions, get_velocities, get_accelerations
 from src.input_manager import parse_input, InputType
 from src.integrate import cumulative_integrate
@@ -43,16 +43,20 @@ if __name__ == '__main__':
 
     start_time = time.time()
 
-    # input pats
+    #input path
     parking_fullinertial_unmod = 'tests/test_fixtures/parking.tsv'
-    # parse input
+    crash = False
     times, coordinates, altitudes, gps_speed, heading, accelerations, angular_velocities = parse_input(
         parking_fullinertial_unmod, [InputType.UNMOD_FULLINERTIAL])
+    converts_measurement_units(accelerations, angular_velocities, gps_speed, coordinates, heading)
+    period = times[1] - times[0]
 
-    converts_measurement_units(accelerations, angular_velocities, gps_speed, coordinates)
+    times, gps_speed, accelerations, angular_velocities, coordinates, heading = \
+        truncate_if_crash(crash, times, gps_speed, accelerations, angular_velocities, coordinates, heading)
 
-    # GNSS data handling
-    gnss_positions, headings = get_positions(coordinates, altitudes)
+    # get positions from GNSS data
+    gnss_positions, headings_2 = get_positions(coordinates, altitudes)
+
     gnss_distance = np.linalg.norm(np.array([gnss_positions[:, i] - gnss_positions[:, i - 1]
                                    for i, x in enumerate(gnss_positions[:, 1:].T, 1)]), axis=1).cumsum()
     # insert initial distance
@@ -62,21 +66,26 @@ if __name__ == '__main__':
     real_velocities = get_velocities(times, gnss_positions)
     real_acc = get_accelerations(times, real_velocities)
 
+    # scalar speed from GNSS position (better than from dataset because avoids Kalmar filter)
     real_velocities_module = np.linalg.norm(real_velocities, axis=0)
 
-    stationary_times = get_stationary_times(gps_speed)
+    stationary_times = get_stationary_times(gps_speed,period)
     # reduce accelerations disturbance
     times, accelerations = reduce_disturbance(times, accelerations, window_size)
     # reduce angular velocities disturbance
     _, angular_velocities = reduce_disturbance(times, angular_velocities, window_size)
-    # resize other arrays after reduce disturbance
+    # truncate other array to match length of acc, thetas, times array
     real_acc = real_acc[:, round(window_size / 2):-round(window_size / 2)]
     real_velocities = real_velocities[:, round(window_size / 2):-round(window_size / 2)]
     gnss_positions = gnss_positions[:, round(window_size / 2):-round(window_size / 2)]
     gnss_distance = gnss_distance[:, round(window_size / 2):-round(window_size / 2)]
 
+    # clear gyroscope drift
     angular_velocities = clear_gyro_drift(angular_velocities, stationary_times)
+    # set times start to 0
     normalize_timestamp(times)
+
+    # correct z-axis alignment
     accelerations, angular_velocities = correct_z_orientation(accelerations, angular_velocities, stationary_times)
     # remove g
     accelerations[2] -= accelerations[2, stationary_times[0][0]:stationary_times[0][-1]].mean()
@@ -88,6 +97,7 @@ if __name__ == '__main__':
 
     real_velocities_module = np.reshape(real_velocities_module, (1, len(real_velocities_module)))
 
+    # integrate acceleration with gss velocities correction
     correct_velocities_module = cumulative_integrate(times, accelerations_module, real_velocities_module[0, 0],
                                                      adjust_data=real_velocities_module,
                                                      adjust_frequency=1)
